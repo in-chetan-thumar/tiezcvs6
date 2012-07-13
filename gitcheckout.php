ce7aba9f739deb555922739f62c765ac5138e40d
in-chetan-thumar <chetan.thumar@tiez.nl>
2012-07-13 12:44:31 +0200

    modify gitcheckout file

--- gitcheckout.php
+++ gitcheckout.php
@@ -1,18 +1,44 @@
 <?php
-	exec('/www/cronjobs/tiezcvs6');
-	echo exec('git pull');
+$action = '';
+if($_POST['action'] != ''){
+	$action = $_POST['action'];
+} 
+$cBranch = shell_exec('git branch | grep "*" | sed "s/* //"');
+
+if($action == ''){
+	echo shell_exec('git pull');
 	
-	//	echo '<br><Br> -l: '.exec('git branch -l');
-	echo '<br><Br> -a: '. $allBranch = shell_exec('git branch -a');
-	echo '<br> array ';
+	$allBranch = shell_exec('git branch -a');
 	$allBranch = explode(' ', $allBranch);
-	print_R($allBranch); 
+	foreach($allBranch as $key => $value){
+		if($value == ''){
+			unset($allBranch[$key]);
+		}elseif($value == '->'){
+			unset($allBranch[$key]);
+		}else{
+			$allBranch[$key] = str_replace('remotes/origin/', '', $value);
+			$allBranch[$key] = str_replace('*', '', $allBranch[$key]);
+			$allBranch[$key] = str_replace('origin/', '', $allBranch[$key]);
+		}
+	}
+	$allBranch = array_unique($allBranch);
+	sort($allBranch);
 	
-	$cBranch = exec('git branch | grep "*" | sed "s/* //"');
-	echo '<br>Current branch name is: ' . $cBranch;
-	
-	$lBranch = exec('git branch --contains');
-	echo '<br><br>Latest branch name is: ' . $lBranch;
-	
-	//echo '<br><br> New branch change is: <bR><br>' . exec('git diff --name-status branch1..branch2'); 
+	echo '<br>Current branch name is: <b>' . $cBranch . '</b>';
+	echo '<form name="checkout" action="" method="post">';
+	echo '<input type="hidden" name="action" value="commitedFileList">';
+	echo '<br><br>Checkout on branch: ';
+		echo '<select name="checkoutBranch">';
+			foreach($allBranch as $key => $value){
+				echo '<option value="'.$value.'">'.$value.'</option>';
+			}
+		echo '</select>';
+		echo '<input type="submit" name="submit" value="Checkout Branch">';
+	echo '</form>';
+}
+print_r($_POST);
+if($action == 'commitedFileList'){
+	echo 'git diff --name-status '.$cBranch.'  '.$_POST['checkoutBranch'].'<BR><BR>';
+	echo '<br><br> New branch change is: <bR><br>' . shell_exec('git diff --name-status '.$cBranch.'  '.$_POST['checkoutBranch']); 
+}
 ?>
