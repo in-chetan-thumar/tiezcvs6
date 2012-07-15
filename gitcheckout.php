76f348967b23f9e28e26d10e56bad737f9fafac5
in-chetan-thumar <chetan.thumar@tiez.nl>
2012-07-15 05:19:15 +0200

    new change

--- gitcheckout.php
+++ gitcheckout.php
@@ -53,13 +53,15 @@ if($action == 'commitedFileList'){
 		echo '<input type="hidden" name="branchName" value="'.$sBranch.'">';
 		echo '<input type="submit" name="submit" value="Checkout Branch">';
 	echo '</form>';
-}
+} 
 if($action == 'checkoutBranch'){
 	$sBranch = trim($_POST['branchName']);
 	echo 'git checkout '.$sBranch;
-	$result = shell_exec('git checkout branch3');
+	$result = shell_exec('git checkout '.$sBranch);
+	echo "<pre>$result</pre>";
+	echo "<br> cd /www/cronjobs/tiezcvs6; git fetch origin; git reset —hard; git checkout ".$sBranch."; git pull origin ".$sBranch.";";
+	$result1 = shell_exec("cd /www/cronjobs/tiezcvs6; git fetch origin; git reset —hard; git checkout ".$sBranch."; git pull origin ".$sBranch.";"); 
+	echo "<pre>$result1</pre>";
 	$result1 = exec('git checkout '.$sBranch);
-	echo $result; 
-	echo $result1; 
 }
 ?>
